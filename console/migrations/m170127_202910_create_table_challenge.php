8c950d40151dea543fa90682d72c0d89a49c3844
Hazrat Ali <iloveyii@yahoo.com>
2018-01-14 23:11:21 +0100

    Fix in default values

--- console/migrations/m170127_202910_create_table_challenge.php
+++ console/migrations/m170127_202910_create_table_challenge.php
@@ -9,9 +9,9 @@ class m170127_202910_create_table_challenge extends Migration
         $this->createTable('challenge', [
             'id' => $this->primaryKey(),
             'sub_category_id' => $this->string(45)->notNull(),
-            'date_start' => $this->timestamp(),
-            'date_stop' => $this->timestamp(),
-            'description' => $this->timestamp(200),
+            'date_start' => $this->timestamp()->defaultValue(null),
+            'date_stop' => $this->timestamp()->defaultValue(null),
+            'description' => $this->string(200),
             'expected_result' => $this->string(50),
         ]);
     }
